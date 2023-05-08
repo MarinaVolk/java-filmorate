13d8f2377d0ad97b6309abcaf7fa1f3f5e8bdbb0
Marina <marvolkova237@yandex.ru>
2023-05-09 01:48:57 +0300

    +

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;/* # parse("File Header.java")*
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.validator.FilmValidator;
 
