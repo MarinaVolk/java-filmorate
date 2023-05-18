e66c25c422b50ac1e3ee43c50f0f889608d60de4
Marina <marvolkova237@yandex.ru>
2023-05-18 19:28:33 +0300

    Commit 1

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,10 +5,15 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     public Film add(Film film);
+
     public Film update(Film film);
+
     public void delete(Integer id);
+
     public List<Film> getAllFilms();
+
     public Film getFilmById(Integer id);
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface UserStorage {
     public User addUser(User user);
+
     public User updateUser(User user);
+
     public void deleteUser(Integer id);
+
     public List<User> getAllUsers();
+
     public User getUserById(Integer id);
 }
