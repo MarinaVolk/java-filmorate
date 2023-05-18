fafa6da0b26b719e4e9d67b26ba37c8921210a01
Marina <marvolkova237@yandex.ru>
2023-05-18 23:40:17 +0300

    popular films

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -81,7 +81,8 @@ public class FilmController {
     @GetMapping("/popular")
     public List<Film> topFilms(@RequestParam(required = false, defaultValue = "10") int count) {
         log.info("Получен запрос на получение топ фильмов count = {}", count);
-        List<Film> topCountFilms = filmService.getTopFilms()
+        List<Film> topCountFilms = new ArrayList<>(films.values())
+        //List<Film> topCountFilms = filmService.getTopFilms()
         .stream()
                 .limit(count)
                 .collect(Collectors.toList());
