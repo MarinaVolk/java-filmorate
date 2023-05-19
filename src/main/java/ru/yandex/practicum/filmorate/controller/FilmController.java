fde89455d28f8e627a6434c2558bee7c4105ab7f
Marina <marvolkova237@yandex.ru>
2023-05-19 13:18:20 +0300

    +

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
