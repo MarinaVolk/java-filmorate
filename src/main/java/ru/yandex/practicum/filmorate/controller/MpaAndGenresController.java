70e93572091b80a3bff83f6fd7918fdb10998c91
Marina <marvolkova237@yandex.ru>
2023-05-27 02:05:40 +0300

    ++

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaAndGenresController.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.controller;/* # parse("File Header.java")*/
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.List;
+
+/**
+ * File Name: MpaAndGenresController.java
+ * Author: Marina Volkova
+ * Date: 2023-05-27,   2:01 AM (UTC+3)
+ * Description:
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class MpaAndGenresController {
+
+    private final FilmService filmService;
+
+    @GetMapping("/mpa/{id}")
+    public Mpa getMpaById(@PathVariable int id) {
+        return filmService.getMpaById(id);
+    }
+
+    @GetMapping("/mpa")
+    public List<Mpa> getAllMpa() {
+        return filmService.findAllMpa();
+    }
+
+    @GetMapping("/genres")
+    public List<Genre> getAllGenres() {
+        return filmService.findAllGenres();
+    }
+
+    @GetMapping("/genres/{id}")
+    public Genre getGenreById(@PathVariable int id) {
+        return filmService.getGenreById(id);
+    }
+
+}
